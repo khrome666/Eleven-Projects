db9905a74b2908fa019f2db0f843a81fda4739c0
khrome666 <khrome666@gmail.com>
2021-02-11 12:04:18 +0000

    First commit

---
+++ Word Count/word_count.py
@@ -0,0 +1,6 @@
+again = True
+while again:
+    on_your_mind = input('What\' on you mind today?')
+    words = on_your_mind.split()
+    print(f'Oh, nice! You just told me what\'s on your mind in {len(words)} word{"" if len(words) == 1 else "s"}!')
+    again = input('Again (y/n)?').lower()[0] == 'y'
