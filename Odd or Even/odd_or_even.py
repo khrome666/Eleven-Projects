836aa182d56325c3856818ac041d0603a1fea0d8
khrome666 <khrome666@gmail.com>
2021-02-11 12:04:03 +0000

    Final version

--- Odd or Even/odd_or_even.py
+++ Odd or Even/odd_or_even.py
@@ -4,7 +4,7 @@ while again:
         print('What number are you thinking?')
         number = int(input())
         odd_or_even_number = 'odd' if number % 2 else 'even'
-        print(f'That\'s an {odd_or_even_number} number. Have another?')
+        print(f'That\'s an {odd_or_even_number} number. Have another go (y/n)?')
         again = input()[0].lower() == 'y'
     except:
         print('That is not a valid number! Try again.')
